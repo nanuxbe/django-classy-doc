377c913ef6c2d0c82b16e0142980a4576ba7cf96
Emmanuelle Delescolle <emma@levit.be>
2024-07-26 12:13:31 +0200

    :bookmark: v0.0.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django_classy_doc',
-    version='0.0.6',
+    version='0.0.7',
     packages=['django_classy_doc'],
     include_package_data=True,
     license='MIT License',  # example license
